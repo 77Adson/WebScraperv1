235c2e597b0c07f242c63c50541bee4f397631fb
77Adson <11maczupaczu@gmail.com>
2025-11-26 19:55:31 +0100

    random interval

--- rate_limiter.py
+++ rate_limiter.py
@@ -1,18 +1,21 @@
 import time
+import random
 from urllib.parse import urlparse
 
 class RateLimiter:
-    def __init__(self, delay=5):
-        self.delay = delay
+    def __init__(self, min_delay=1, max_delay=5):
+        self.min_delay = min_delay
+        self.max_delay = max_delay
         self.last_request_times = {}
 
     def wait(self, url):
         domain = urlparse(url).netloc
         last_time = self.last_request_times.get(domain, 0)
         elapsed = time.time() - last_time
-        if elapsed < self.delay:
-            time.sleep(self.delay - elapsed)
+        delay = random.uniform(self.min_delay, self.max_delay)
+        if elapsed < delay:
+            time.sleep(delay - elapsed)
         self.last_request_times[domain] = time.time()
 
 # Global instance
-limiter = RateLimiter(delay=2)
+limiter = RateLimiter(min_delay=1, max_delay=5)
